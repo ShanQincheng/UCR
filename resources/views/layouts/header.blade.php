971e328133bb67b8a9be46a141f205c5160b59da
shanqincheng <codingstory@outlook.com>
2022-05-23 23:39:19 +1000

    install Laravel breeze starter-kit

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
+    <title>UTAS Computer Rental Services Homepage</title>
+</head>
+<body>
+<!-- Bootstrap .navbar-brand for your company, product, or project name. -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <!-- As a Heading -->
+        <span class="navbar-brand mb-0 h1">UCR</span>
+        <!-- As a link -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <!-- Add the .active class on .nav-link to indicate the current page.
+                     Please note that you should also add the aria-current attribute on the active .nav-link.-->
+                <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+            </div>
+        </div>
+    </div>
+</nav>
