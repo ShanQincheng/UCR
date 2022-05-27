a1a5d82c7dd43893c9736c04b813b3042ca9e8f8
shanqincheng <codingstory@outlook.com>
2022-05-27 22:16:36 +1000

    use Carousel replace homepage picture

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,27 @@
     <div class="container-fluid">
         <div class="row row-cols-2">
             <div class="col-9" id="home-welcome-box">
-                <img src = "https://cdn.jsdelivr.net/gh/cat-imado/imado-s-live@main/UTAS/UCR-Rental-Main-Page-Welcome-Picture.png"
-                     class="img-fluid" id="home-welcome-pic" alt = "UCR Rental Welcome Picture">
+                <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="https://cdn.jsdelivr.net/gh/cat-imado/imado-s-live@main/UTAS/urc-homepage-welcome-pic-3.png" class="img-fluid" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://cdn.jsdelivr.net/gh/cat-imado/imado-s-live@main/UTAS/urc-homepage-welcome-pic-2.png" class="img-fluid" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://cdn.jsdelivr.net/gh/cat-imado/imado-s-live@main/UTAS/urc-homepage-welcome-pic-5.png" class="img-fluid" alt="...">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
             </div>
             <div class="col-3">
                 @guest
